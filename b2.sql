0f52a1953c1ae46abeab82ae0166d1b0f35d2167
trung2007hs-jpg <trung2007hs@gmail.com>
2026-05-06 15:54:08 +0700

    Add files via upload

---
+++ b2.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS AddInventory;
+DELIMITER $$
+CREATE PROCEDURE AddInventory(IN p_item_id INT, IN p_quantity INT)
+BEGIN
+    -- Kiểm tra điều kiện: Số lượng nhập phải lớn hơn 0
+    IF p_quantity > 0 THEN
+        -- Thực hiện cộng dồn kho
+        UPDATE Inventory
+        SET stock_quantity = stock_quantity + p_quantity
+        WHERE item_id = p_item_id;
+        SELECT 'Thành công: Đã cập nhật số lượng nhập kho.' AS Message;
+    ELSE
+        -- Thông báo lỗi nếu số lượng không hợp lệ
+        SELECT 'Lỗi: Số lượng nhập kho phải lớn hơn 0!' AS Message;
+    END IF;
+END $$
